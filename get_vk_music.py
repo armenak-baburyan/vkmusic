44b940c62c850f0d9f2cbbb898db508f297fb995
Armenak Baburyan <ababuryan@msk.action.ru>
2014-05-20 11:50:05 +0400

    tags update

--- get_vk_music.py
+++ get_vk_music.py
@@ -2,21 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import glob
 import json
 import os
+import re
 import shelve
 import shutil
 import time
 import urllib.request
 import webbrowser
 from concurrent import futures
+from html import unescape
 from urllib.parse import urlparse, parse_qs
+
+from mutagenx.id3 import ID3NoHeaderError
+from mutagenx.id3 import ID3, TALB, TIT2, TPE1, TPE2
 
 
 APP_ID = '3889070'
 APP_SCOPE = 'audio,offline'
 AUTH_FILE = '.auth_data'
 OUTPUT_FOLDER = 'vk_music'
+ALBUM = 'vkMusic'
 
 
 class APIException(Exception):
@@ -70,13 +77,13 @@ class Authorization():
 
 
 class UserMusic(object):
-    def __init__(self, uid, access_token, output_folder):
+    def __init__(self, uid, access_token, output_folder, album):
         self.uid = uid
         self.access_token = access_token
-
         self.output_folder = output_folder
         if not os.path.exists(self.output_folder):
             os.makedirs(self.output_folder)
+        self.album = album
 
         self.aids = {x[:-4] for x in os.listdir(self.output_folder)}
 
@@ -88,36 +95,62 @@ class UserMusic(object):
         content = response.read()
         self._content = json.loads(content.decode('utf-8'))
         self.music_list = self._content['response']
+        self.map = {str(track['aid']): {'artist': track['artist'], 'title': track['title']} for track in self.music_list}
 
     def download(self):
         with futures.ProcessPoolExecutor(max_workers=4) as executor:
-            executor.map(self.get_track, reversed(self.music_list))
+            executor.map(self._get_track, reversed(self.music_list))
 
-    def get_track(self, track):
+    def _get_track(self, track):
         track_name = '{artist} - {title}'.format(**track)
         if str(track['aid']) in self.aids:
-            print('skipped: ', track_name)
             return
 
         with urllib.request.urlopen(track['url']) as track_resp,\
                 open(os.path.join(self.output_folder, '{}.mp3'.format(track['aid'])), 'wb') as out_file:
             shutil.copyfileobj(track_resp, out_file)
         self.aids.add(track['aid'])
-        print(track_name, '-->', out_file.name)
+        print('\u2705', track_name, '-->', out_file.name)
 
+    def update_tags(self):
+        with futures.ProcessPoolExecutor(max_workers=4) as executor:
+            executor.map(self._update_track_tags, glob.glob(os.path.join(self.output_folder, '*.mp3')))
+
+    def _update_track_tags(self, fname):
+        pattern = re.compile(r'.*/(\d+).mp3')
+        m = re.search(pattern, fname)
+        aid = m.group(1)
+        artist = unescape(self.map[aid]['artist'])
+        title = unescape(self.map[aid]['title'])
+
+        # http://stackoverflow.com/a/14040318/1886653
+        try:
+            tags = ID3(fname)
+        except ID3NoHeaderError:
+            tags = ID3()
+
+        tags.delete()
+        tags["TALB"] = TALB(encoding=3, text=self.album)
+        tags["TIT2"] = TIT2(encoding=3, text=artist)
+        tags["TPE1"] = TPE1(encoding=3, text=title)
+        tags["TPE2"] = TPE2(encoding=3, text=self.album)
+        tags.save(fname)
+        print('\u2705', fname, '-->', '{} - {} - {}'.format(self.album, artist, title))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--destination', action='store', dest='output_folder',
                         default=OUTPUT_FOLDER, help='Output folder')
+    parser.add_argument('-a', '--album', action='store', dest='album',
+                        default=ALBUM, help='Album title')
     cli_args = parser.parse_args()
 
     auth = Authorization(APP_ID, APP_SCOPE)
 
-    music = UserMusic(auth.uid, auth.access_token, output_folder=cli_args.output_folder)
-    print('Downloading Your vk Music Collection.')
     t = time.time()
+    music = UserMusic(auth.uid, auth.access_token, output_folder=cli_args.output_folder, album=cli_args.album)
+    print('*' * 80, 'Downloading New Songs from Your vk Music Collection.', '*' * 80, sep='\n')
     music.download()
-    print('Download Complete')
-    print('Processing Time: {} seconds'.format(time.time() - t))
-
+    print('*' * 80, 'Updating Songs Tags', '*' * 80, sep='\n')
+    music.update_tags()
+    print('#' * 80, 'Processing Time: {} seconds'.format(time.time() - t), sep='\n')
